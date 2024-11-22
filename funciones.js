6b2c97eecabd4842dbc11f10071317157e0d5e5b
aingar <ainhoacalabuiggarcia@gmail.com>
2024-11-22 18:20:55 +0100

    palabra secreta no va aun

--- funciones.js
+++ funciones.js
@@ -1,4 +1,4 @@
-let words = null
+let words
 
 function LeerArchivoPalabras(){
     fetch("words.json")
@@ -14,21 +14,48 @@ function LeerArchivoPalabras(){
     })
 }
 
+/* SOLO 1 LETRA TANTO MAYUSCULA COMO MINUSCULA --> poner TRY-CATCH*/
+
 function validarDatos(e){
+    const palabraSecreta =document.getElementById("txtLetra")
+    //validaciones mediante RegEx
+    if(!txtLetra.value.match(/[a-z]/)){ //si es diferente de una letra: ERROR
+        Swal.fire({
+            title: "Error!",
+            text: "Tienes que introducir una sola letra.",
+            icon: "error"
+          });
+    } else { //si es una letra
+
+    }
 
 }
 
 function cambiarImagen(){
     const imgInicial = document.getElementById("imgInicial")
-    //ganaPierde.innerText = "..que tengo aquí colgado!"
-    //ganaPierde.style.color = "red"
 
-    /*} else {
-        ganaPierde.innerText = "...a quén has salvado!"
+    /*if(la imagen es la 8){ 
+        ganaPierde.innerText = "..que tengo aquí colgado!"
+        ganaPierde.style.color = "red"
+    } else {
+        ganaPierde.innerText = "...a quién has salvado!"
         //ganaPierde.style.color = "green"
     }*/
 }
 
-function crearMatriz(){
+function palabraSecreta(){
+    /*let word
+    word = words[Math.floor(Math.random() * words.length)] + 0; //Random para obtener una palabra aleatoria desde el array
+    //El 0 es el mínimo rango que puede retornar
+    return word;
+    */
+   for(let palabra of words){
     
+   }
 }
+
+console.log(palabraSecreta());
+
+/*word = Math.floor(Math.random()*words.length-1)
+document
+*/
